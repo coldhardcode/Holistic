22a7d76889dc87077a6613f4a94698c1778ebaf9
Jay Shirley <j@shirley.im>
2010-02-13 16:50:11 -0800

    normalization

--- lib/Holistic/Controller/What.pm
+++ lib/Holistic/Controller/What.pm
@@ -1,41 +1,38 @@
 package Holistic::Controller::What;
 
-use parent 'Catalyst::Controller';
+use parent 'Holistic::Base::Controller';
 
 use Moose;
 
-sub base : Chained('../what') PathPart('what') CaptureArgs(0) {
+sub setup : Chained('.') PathPart('what') CaptureArgs(0) {
     my ( $self, $c ) = @_;
 
 }
 
-sub default : Chained('base') PathPart('') Args(0) {
+sub root : Chained('setup') PathPart('') Args(0) {
     my ($self, $c) = @_;
-
-    $c->stash->{template} = 'who/default.tt';
 }
 
-sub person : Chained('base') PathPart('person') Args(1) {
+sub person : Chained('setup') PathPart('person') Args(1) {
     my ($self, $c, $person) = @_;
 
     $c->stash->{object} = 'person';
     $c->stash->{template} = 'what/object.tt';
 }
 
-sub product : Chained('base') PathPart('product') Args(1) {
+sub product : Chained('setup') PathPart('product') Args(1) {
     my ($self, $c, $person) = @_;
 
     $c->stash->{object} = 'product';
     $c->stash->{template} = 'what/object.tt';
 }
 
-sub queue : Chained('base') PathPart('queue') Args(1) {
+sub queue : Chained('setup') PathPart('queue') Args(1) {
     my ($self, $c, $person) = @_;
 
     $c->stash->{object} = 'queue';
     $c->stash->{template} = 'what/object.tt';
 }
-
 
 
 no Moose;
