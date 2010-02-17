6c4f161b761d18db31be9d3a191c9cc439bf39c6
Jay Shirley <j@shirley.im>
2010-02-17 07:43:23 -0800

    Group management hacking

--- lib/Holistic/Controller/Admin/Group.pm
+++ lib/Holistic/Controller/Admin/Group.pm
@@ -9,9 +9,69 @@ __PACKAGE__->config(
     class      => 'Schema::Group',
     rs_key     => 'group_rs',
     object_key => 'group',
+    prefetch   => [ 'person_links' ]
 );
 
-sub management : Chained('setup') Args(0) { }
+sub management : Chained('setup') Args(0) ActionClass('REST') { }
+
+sub management_GET { 
+    my ( $self, $c ) = @_;
+    my $rs = $c->model('Schema::Group')->search(
+        {
+                #'me.active' => 1, # TODO
+        },
+        {
+            prefetch => [ 'person_links' ]
+        }
+    );
+    $rs->result_class('DBIx::Class::ResultClass::HashRefInflator');
+    $c->stash->{person_groups} = {};
+    foreach my $row ( $rs->all ) {
+        my $links = delete $row->{person_links};
+        foreach my $link ( @$links ) {
+            $c->stash->{person_groups}->{$link->{person_pk1}} ||= [];
+            push @{ $c->stash->{person_groups}->{$link->{person_pk1}} }, {
+                group_pk1 => $row->{pk1},
+                name      => $row->{name},
+                role_pk1  => $link->{role_pk1},
+            };
+        }
+    }
+
+    $self->status_ok(
+        $c,
+        entity => {
+            groups => [ $rs->all ]
+        }
+    );
+}
+
+sub management_POST {
+    my ( $self, $c, $data ) = @_;
+    $data ||= $c->req->data || $c->req->params;
+
+    my @p_pk1s = ref $data->{person_pk1} ?
+        @{ $data->{person_pk1} } : ( $data->{person_pk1} );
+
+    my @g_pk1s = ref $data->{group_pk1} ?
+        @{ $data->{group_pk1} } : ( $data->{group_pk1} );
+
+    my $role = $c->model('Schema')->schema->get_role('Member');
+
+    my @groups = $c->model('Schema::Group')->search({ pk1 => \@g_pk1s })->all;
+
+    foreach my $pk1 ( @p_pk1s ) {
+        my $person = $c->model('Schema::Person')->find($pk1);
+        if ( not defined $person ) {
+            $c->log->error("$pk1 is not a valid person id");
+            next;
+        }
+        $person->group_links->delete;
+        foreach my $group ( @groups ) {
+            $group->add_to_persons( $person, { role_pk1 => $role->id });
+        }
+    }
+}
 
 no Moose;
 __PACKAGE__->meta->make_immutable( inline_constructor => 0 );
--- root/static/scripts/group_manager.js
+++ root/static/scripts/group_manager.js
@@ -1,11 +1,50 @@
 YUI().use("event-delegate", function(Y) {
+    function updateGroups(person_pk1) {
+        Y.all('#user_groups input[name="group_pk1"]').set('checked', false);
+        var groups = group_config[person_pk1];
+        if ( typeof groups === 'undefined' ) {
+            Y.log("No groups for " + person_pk1);
+            return;
+        }
+        for ( var i = 0; i < groups.length; i++ ) {
+            Y.one('#group_group' + groups[i].group_pk1).set('checked', true);
+        }
+
+    }
+
+    Y.delegate('change', function(e) {
+        var remove    = e.currentTarget.get('checked') !== true; 
+        var group_pk1 = e.currentTarget.get('value');
+        var list      = Y.all('#user_groups .people input');
+
+        list.each(
+            function(node, index, nodeList) {
+                if ( node.get('checked') !== true )
+                    return;
+                var person_pk1 = node.get('value');
+
+                var group_list = Y.all('#user_groups .groups input');
+                var selected_list = [];
+                group_list.each( function(group_node) {
+                    if ( group_node.get('checked') ) {
+                        selected_list.push({ group_pk1: group_node.get('value') });
+                    }
+                } );
+
+                group_config[person_pk1] = selected_list;
+            }
+        );
+    }, '#user_groups', 'input[name="group_pk1"]');
+
     Y.delegate('change', function(e) {
         var li = e.currentTarget.get('parentNode').get('parentNode');
         if ( e.currentTarget.get('type').toUpperCase() === 'RADIO' )
             li.get('parentNode').get('children').removeClass('selected');
-
-        if ( e.currentTarget.get('checked') === true )
+        if ( e.currentTarget.get('checked') === true ) {
             li.addClass('selected');
+            updateGroups(e.currentTarget.get('value'));
+        } else
+            li.removeClass('selected');
     }, '#group_management_users', 'ul.people li input'); 
 });
 
