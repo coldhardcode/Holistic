92c06d6e33f9963044c247523a051e2f93be9ee9
Jay Shirley <j@shirley.im>
2010-02-21 11:35:03 -0800

    Placeholder support

---
+++ root/static/scripts/placeholder.js
@@ -0,0 +1,34 @@
+YUI().use("node", function(Y) {
+    // webkit has native placeholder support
+    if ( Y.UA.webkit ) return;
+
+    Y.all('form').each( function() {
+        var form = this;
+        form.all('input[placeholder]').each( function() {
+            if ( this.get('value') === '' ) {
+                this.addClass('placeholder');
+                this.set('value', this.getAttribute('placeholder'));
+            }
+
+            this.on('focus', function() {
+                if ( this.get('value') === this.getAttribute('placeholder') ) {
+                    this.toggleClass('placeholder');
+                    this.set('value', '');
+                }
+            });
+            this.on('blur', function() {
+                if ( this.get('value').length < 1 ) {
+                    this.toggleClass('placeholder');
+                    this.set('value', this.getAttribute('placeholder'));
+                }
+            });
+        });
+        form.on('submit', function() {
+            form.all('input[placeholder]').each( function() {
+                if ( this.get('value') === this.getAttribute('placeholder') )
+                    this.set('value', '');
+            } );
+        });
+    });
+
+});
