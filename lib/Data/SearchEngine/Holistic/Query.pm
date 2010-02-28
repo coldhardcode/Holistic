5faabc1c83aef4ae0208414669254d5141110ee6
Cory Watson <github@onemogin.com>
2010-02-28 17:50:58 -0600

    Some search stuff

--- lib/Data/SearchEngine/Holistic/Query.pm
+++ lib/Data/SearchEngine/Holistic/Query.pm
@@ -1,6 +1,23 @@
 package Data::SearchEngine::Holistic::Query;
 use Moose;
+use MooseX::UndefTolerant;
+use Moose::Util::TypeConstraints;
 
 extends 'Data::SearchEngine::Query';
+
+coerce 'ArrayRef'
+    => from 'Str'
+    => via { [ int($_) ] };
+
+coerce 'ArrayRef'
+    => from 'Num'
+    => via { [ int($_) ] };
+
+has requester => (
+    is => 'ro',
+    isa => 'ArrayRef',
+    coerce => 1,
+    predicate => 'has_requestor'
+);
 
 1;
