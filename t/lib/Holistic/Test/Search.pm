88b82b2c5662686407bd7a28412edf18723f8c58
Jay Shirley <j@shirley.im>
2010-03-02 15:12:00 -0800

    Is too

---
+++ t/lib/Holistic/Test/Search.pm
@@ -0,0 +1,44 @@
+package Holistic::Test::Search;
+
+use Test::More;
+use Moose::Role;
+use MooseX::MethodAttributes::Role;
+
+use Try::Tiny;
+
+use Data::SearchEngine::Holistic;
+use Data::SearchEngine::Holistic::Query;
+
+with 'Holistic::Test::Schema'; # We require schema
+
+has 'searcher' => (
+    is => 'rw',
+    lazy_build => 1,
+    handles => {
+        'search' => 'search'
+    }
+);
+
+sub _build_searcher {
+    Data::SearchEngine::Holistic->new( schema => shift->schema );
+}
+
+sub do_search : Plan(1) { 
+    my ( $self, $data ) = @_;
+
+    my $q     = $data->{query} || 'test';
+    my $page  = 1;
+    my $count = 10;
+
+    my $query = Data::SearchEngine::Holistic::Query->new(
+        original_query  => $q,
+        query           => $q,
+        page            => $page,
+        count           => $count
+    );
+
+    my $results = $self->search( $query );
+    ok($results, 'got results');
+}
+
+1;
---
+++ t/search.t
@@ -0,0 +1,23 @@
+use Holistic::Test::Suite;
+
+my $suite = Holistic::Test::Suite->new;
+
+$suite->run(
+    #with => [ 'Person', 'Group', 'Ticket' ],
+    with => [ 'Person', 'Queue', 'Ticket', 'Search' ],
+    config => {
+        connect_info => [
+            'dbi:SQLite:t/var/test.db',
+            '', '',
+            { quote_char => '`', name_sep => '.' }
+        ]
+    },
+    tests => [
+        'deploy',
+        'ticket_create',
+        'ticket_dependencies',
+        'ticket_profile',
+        'do_search',
+    ]
+);
+
