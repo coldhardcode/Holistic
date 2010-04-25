a05dc8db78526d35ed6940a2c334e4eeffb9264b
Jay Shirley <j@shirley.im>
2010-04-25 14:43:12 -0700

    Assignments object, with terrible naming that needs to be fixed

--- lib/Holistic/Ticket.pm
+++ lib/Holistic/Ticket.pm
@@ -3,6 +3,8 @@ use Moose;
 
 use Holistic::Types qw(TicketPriority TicketType);
 
+use Holistic::Ticket::Assignments;
+
 has 'date_due' => (
     is => 'rw',
     isa => 'DateTime'
@@ -36,4 +38,23 @@ has 'type' => (
     default => 'Defect'
 );
 
-1;
+has 'people' => (
+    is      => 'rw',
+    isa     => 'Holistic::Ticket::Assignments',
+    lazy_build => 1,
+    handles => {
+        'all_owners' => 'all_owners',
+        'add_owners' => 'add_owners',
+        'all_cc'     => 'all_cc',
+        'add_cc'     => 'add_cc',
+        'all_requestors' => 'all_requestors',
+        'add_requestor'  => 'add_requestor',
+        'all_attention'  => 'all_attention',
+        'add_attention'  => 'add_attention',
+    }
+);
+
+sub _build_people { Holistic::Ticket::Assignments->new }
+
+no Moose;
+__PACKAGE__->meta->make_immutable;
