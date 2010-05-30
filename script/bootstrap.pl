d3b8f10280eb6e1659dcc43fe071d47d0c3279e7
Jay Shirley <j@shirley.im>
2010-05-30 15:44:28 -0700

    Slightly less dumb bootstrap script, but still terrible.

--- script/bootstrap.pl
+++ script/bootstrap.pl
@@ -14,8 +14,8 @@ has [ 'username', 'password' ] => (
 
 package main;
 
-
 use Holistic;
+use Try::Tiny;
 
 my $um = Holistic::Bootstrap->new_with_options;
 
@@ -38,14 +38,16 @@ $schema->txn_do( sub {
     });
     my $pset = $authenticated->permission_set;
     foreach my $permission ( qw/TICKET_CREATE TICKET_MODIFY/ ) {
-        $pset->add_to_permissions( $prs->find_or_create({ name => $permission }) );
+        try { $pset->add_to_permissions( $prs->find_or_create({ name => $permission }) ); }
+        catch { die $_ unless "$_" =~ /Duplicate/ };
     }
     my $admins = $schema->resultset('Group')->find_or_create({
         name => 'Admin',
     });
     $pset = $admins->permission_set;
     foreach my $permission ( qw/TICKET_CREATE TICKET_MODIFY ADMIN/ ) {
-        $pset->add_to_permissions( $prs->find_or_create({ name => $permission }) );
+        try { $pset->add_to_permissions( $prs->find_or_create({ name => $permission }) ); }
+        catch { die $_ unless $_ =~ /Duplicate/ };
     }
 
     $admins->add_to_persons( $admin, { role_pk1 => $role->id });
